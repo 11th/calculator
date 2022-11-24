42ad74b6c99dcf47f19b1231964ecf7e12d3480c
Edinichkin <11th@bk.ru>
2022-11-24 14:44:52 +0300

    Homework is done

--- src/test/java/com/eleventh/calculator/services/CalculatorServiceTest.java
+++ src/test/java/com/eleventh/calculator/services/CalculatorServiceTest.java
@@ -9,52 +9,52 @@ public class CalculatorServiceTest {
     private CalculatorService out = new CalculatorService();
 
     @Test
-    public void correctGreet(){
+    public void shouldReturnCorrectGreet(){
         Assertions.assertEquals(CalculatorServiceTestConstants.GREETING, out.greet());
     }
 
     @Test
-    public void correctSummarize(){
+    public void shouldReturnFourBySummarizeTwoAndTwo(){
         Assertions.assertEquals(4.0, out.summarize(2.0, 2.0));
     }
 
     @Test
-    public void incorrectSummarize(){
-        Assertions.assertNotEquals(4.0, out.summarize(1.0, 2.0));
+    public void shouldReturnOneBySummarizeZeroAndOne(){
+        Assertions.assertEquals(1.0, out.summarize(0.0, 1.0));
     }
 
     @Test
-    public void correctSubtract(){
+    public void shouldReturnZeroBySubtractOneAndOne(){
+        Assertions.assertEquals(0.0, out.subtract(1.0, 1.0));
+    }
+
+    @Test
+    public void shouldReturnOneBySubtractTwoAndOne(){
         Assertions.assertEquals(1.0, out.subtract(2.0, 1.0));
     }
 
     @Test
-    public void incorrectSubtract(){
-        Assertions.assertNotEquals(1.0, out.subtract(1.0, 1.0));
+    public void shouldReturnZeroByMultiplyOneAndZero(){
+        Assertions.assertEquals(0.0, out.multiply(1.0, 0.0));
     }
 
     @Test
-    public void correctMultiply(){
-        Assertions.assertEquals(9.0, out.multiply(3.0, 3.0));
+    public void shouldReturnFourByMultiplyTwoAndTwo(){
+        Assertions.assertEquals(4.0, out.multiply(2.0, 2.0));
     }
 
     @Test
-    public void incorrectMultiply(){
-        Assertions.assertNotEquals(99.0, out.multiply(3.0, 3.0));
+    public void shouldReturnOneByDivideTwoAndTwo(){
+        Assertions.assertEquals(1.0, out.divide(2.0, 2.0));
     }
 
     @Test
-    public void correctDivide(){
-        Assertions.assertEquals(3.0, out.divide(9.0, 3.0));
+    public void shouldReturnZeroByDivideZeroAndTwo(){
+        Assertions.assertEquals(0.0, out.divide(0.0, 2.0));
     }
 
     @Test
-    public void incorrectDivide(){
-        Assertions.assertNotEquals(33.0, out.divide(9.0, 3.0));
-    }
-
-    @Test
-    public void divideByZero(){
-        Assertions.assertThrows(IllegalValueException.class, () -> { out.divide(3.0, 0.0); });
+    public void shouldReturnIlligalValueExceptionByDivideOneAndZero(){
+        Assertions.assertThrows(IllegalValueException.class, () -> { out.divide(1.0, 0.0); });
     }
 }
